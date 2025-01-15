da0e005b5327b2d25ba14440c616ba399262128b
RDX <nasir@source.one>
2025-01-15 13:56:07 +0530

    added more lecture codes.

---
+++ tests/handlingCalendar.spec.js
@@ -0,0 +1,25 @@
+import { expect, test } from "@playwright/test";
+
+test("Calendar validations", async ({ page }) => {
+  const monthNumber = "6";
+  const date = "15";
+  const year = "2027";
+  const expectedList = [monthNumber, date, year];
+  await page.goto("https://rahulshettyacademy.com/seleniumPractise/#/offers");
+  await page.locator(".react-date-picker__inputGroup").click();
+  await page.locator(".react-calendar__navigation__label").click();
+  await page.locator(".react-calendar__navigation__label").click();
+  await page.getByText(year).click();
+  await page.locator(".react-calendar__year-view__months__month").nth(Number(monthNumber) - 1).click();
+  await page.locator("//abbr[text()='" + date + "']").click();
+
+  const inputs = await page.locator(".react-date-picker__inputGroup__input").all();
+  const values = await Promise.all(inputs.map(input => input.getAttribute("value")));
+  console.log({ values });
+
+  for (let index = 0; index < values.length; index++) {
+    const value = values[index];
+    console.log(`>> inside for loop, the value received is ${value}, and the expected value is ${expectedList[index]}`);
+    expect(value).toEqual(expectedList[index]);
+  }
+})
