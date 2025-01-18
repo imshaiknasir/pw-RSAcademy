6aa10018da7b8f1042174e71a3befd3cf7008c4e
RDX <nasir@source.one>
2025-01-18 18:52:41 +0530

    All route lectture code added

---
+++ tests/api_tests/ApiTest_ByAbortingNetworkCalls.spec.js
@@ -0,0 +1,17 @@
+import { expect, test } from '@playwright/test'
+
+test('Aborting the Request for the orders view page', async ({ page }) => {
+    // Navigate to the page
+    await page.goto('https://rahulshettyacademy.com/')
+
+    const registerButtonLocator = page.locator("//a[text()='Register']")
+    await expect(registerButtonLocator).toHaveCSS('background-color', 'rgb(236, 82, 82)') // it should have red background color
+
+    // Next, we will do the same check with aborting the request for the css files
+    await page.route('**/*.css', async route => {
+        await route.abort()
+    })
+    await page.reload()
+
+    await expect(registerButtonLocator).not.toHaveCSS('background-color', 'rgb(236, 82, 82)') // it should not have red background color
+})
---
+++ tests/api_tests/ApiTest_ByMockingTheRequest.spec.js
@@ -0,0 +1,48 @@
+import { expect, request, test } from '@playwright/test'
+
+let token = ''
+
+test.beforeAll(async () => {
+    console.log('>>> Start of Before all <<<')
+    const apiContext = await request.newContext()
+    const response = await apiContext.post('https://rahulshettyacademy.com/api/ecom/auth/login', {
+        data: {
+            userEmail: "anshika@gmail.com",
+            userPassword: "Iamking@000"
+        }
+    })
+    expect(response.status()).toBe(200)
+    const responseBody = await response.json()
+    console.log({ responseBody })
+    token = responseBody.token
+    console.log('>>> End of Before all <<<')
+})
+
+test('Mocking the Request for the orders view page', async ({ page }) => {
+    // First we need to inject the token into the page
+    await page.addInitScript(injectToken => {
+        window.localStorage.setItem('token', injectToken)
+    }, token)
+
+    // Then we need to navigate to the page
+    await page.goto('https://rahulshettyacademy.com/client')
+
+    // Assert that we land on the products page
+    const productsLocator = page.locator('//h5/b')
+    await productsLocator.first().waitFor({ state: 'visible' })
+
+    // Lets goto Orders page
+    await page.locator("//button[@routerlink='/dashboard/myorders']").click()
+
+    // Before we view the order, we need to modify the request
+    await page.route(`https://rahulshettyacademy.com/api/ecom/order/get-orders-details?id=*`, async route => {
+        await route.continue({ url: 'https://rahulshettyacademy.com/api/ecom/order/get-orders-details?id=678a69d1e2b5443b1f281570' })
+    })
+
+    // Now lets view the order
+    await page.getByRole('button', { name: 'View', exact: true }).first().click()
+
+    // Assert the error message
+    const errorMessageLocator = page.locator("//p[contains(text(),'You are not authorize to view this order')]")
+    await expect(errorMessageLocator).toBeVisible()
+})
