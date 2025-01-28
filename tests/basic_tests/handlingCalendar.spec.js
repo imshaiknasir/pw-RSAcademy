5322e04e4d4504c54475c6a9fc3760ffb409bd47
RDX <nasir@source.one>
2025-01-28 11:55:38 +0530

    Fix calendar date selection to handle neighboring month dates

--- tests/basic_tests/handlingCalendar.spec.js
+++ tests/basic_tests/handlingCalendar.spec.js
@@ -2,7 +2,7 @@ import { expect, test } from "@playwright/test";
 
 test("Calendar validations", async ({ page }) => {
   const monthNumber = "6";
-  const date = "15";
+  const date = "1";
   const year = "2027";
   const expectedList = [monthNumber, date, year];
   await page.goto("https://rahulshettyacademy.com/seleniumPractise/#/offers");
@@ -11,7 +11,7 @@ test("Calendar validations", async ({ page }) => {
   await page.locator(".react-calendar__navigation__label").click();
   await page.getByText(year).click();
   await page.locator(".react-calendar__year-view__months__month").nth(Number(monthNumber) - 1).click();
-  await page.locator("//abbr[text()='" + date + "']").click();
+  await page.locator(`//abbr[text()='${date}']/parent::button[not(contains(@class,'neighboringMonth'))]`).click(); // to handle the neighboring month dates
 
   const inputs = await page.locator(".react-date-picker__inputGroup__input").all();
   const values = await Promise.all(inputs.map(input => input.getAttribute("value")));
